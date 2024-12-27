8d70c18ccab115037da3bae93ae8120578396878
Akshat Sharma <Akshatsharmain11@gmail.com>
2024-12-27 18:48:30 +0530

    Cab

--- groups.js
+++ groups.js
@@ -4,7 +4,6 @@ const group = {
     },
     'wintersem2425': {
         'Link': 'https://docs.google.com/spreadsheets/d/1xItL90XfVk1qAu95FBm7VwmhwNFPwG_M56b4Af1wGPI/edit?usp=sharing',
-        'Add' : 'https://forms.gle/o6QzUCU91EMpfgfT8',
     }
 };
 
@@ -32,10 +31,9 @@ document.getElementById("submitsem").addEventListener('click', function () {
     if (selectedsem in group) {
         let sem = document.getElementById("sem");
         let Link = group[selectedsem]['Link'];
-        let Add = group[selectedsem]['Add'];
         let clickh = `Click Here!`
-        sem.innerHTML = `<a href="${Link}" target="_blank">${Link}</a> <br> <br>;
-                            <a href="${Add}" target="_blank">${Add}</a>`;
+        sem.innerHTML = `<a href="${Link}" target="_blank">${Link}</a> <br> <br>`;
+
     } else {
         let sem = document.getElementById("sem");
         sem.innerHTML = `Please Select a option from the available options`;
@@ -53,9 +51,7 @@ document.getElementById("submitsem").addEventListener('click', function () {
     if (selectedsem in group) {
         let sem = document.getElementById("sem");
         let Link = group[selectedsem]['Link'];
-        let Add = group[selectedsem]['Add'];
-        sem.innerHTML = `To Join and Find Groups: <a href="${Link}" target="_blank">${Link}</a> <br> <br>
-                            To add your own group: <br> <a href="${Add}" target="_blank">${Add}</a>`;
+        sem.innerHTML = `To Join and Find Groups: <a href="${Link}" target="_blank">${Link}</a> <br> <br>`;
     } else {
         let sem = document.getElementById("sem");
         sem.innerHTML = `Please Select a option from the available options`;
